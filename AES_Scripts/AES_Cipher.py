9fc3183429e0ce697c8973c15a05fe4d2d90e3c0
Santuario Parra <lsantuariop1400@alumno.ipn.mx>
2019-05-07 11:35:54 -0500

    Actualizacion 1
    Agregados Scripts de AES funcionando

---
+++ AES_Scripts/AES_Cipher.py
@@ -0,0 +1,60 @@
+import argparse
+
+import Crypto.Cipher.AES as AES
+import Crypto.Random as random
+import base64 as base64
+
+
+def aesc(archivo):
+    key = random.get_random_bytes(16)
+    try:
+        file = open(archivo, 'rb')
+        data = file.read()
+        file.close()
+        cipher = AES.new(key, AES.MODE_CFB)
+        cipherData = cipher.iv + cipher.encrypt(data)
+        file = open(archivo, 'wb')
+        file.write(cipherData)
+        file.close()
+        return base64.standard_b64encode(key)
+    except FileNotFoundError:
+        return "El archivo no existe"
+
+
+def aesd(archivo, key):
+    key = base64.standard_b64decode(key)
+    try:
+        file = open(archivo, 'rb')
+        data = file.read()
+        file.close()
+        iv = data[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CFB, iv)
+        cipherDData = cipher.decrypt(data[AES.block_size:])
+        file = open(archivo, 'wb')
+        file.write(cipherDData)
+        file.close()
+        return "Archivo Descifrado"
+    except FileNotFoundError:
+        return "El archivo no existe"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cifrar", help="Cifra el archivo y devuelve la llave con el archivo cifrado",
+                        action="store_true")
+    parser.add_argument("-d", "--descifrar", help="Descifra el archivo seleccionado",
+                        action="store_true")
+    parser.add_argument("-f", "--file", help="Nombre del archivo")
+    parser.add_argument("-k", "--key", help="Llave base64 del archivo")
+    args = parser.parse_args()
+
+    #key = aesc(args.file)
+    #print(key)
+
+    #salida = aesd(args.file, args.key)
+    #print(salida)
+
+
+if __name__ == '__main__':
+    main()
+
+
